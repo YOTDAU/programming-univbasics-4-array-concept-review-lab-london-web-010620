7e01c0515e2ee88bfb58ea708bb99921511a6993
Simon Tucker <sjtucker28@gmail.com>
2019-12-13 16:01:55 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -14,10 +14,9 @@ def find_max_value(array)
   array.length.times {|index|
   if n < array[index]
     n = array[index]
-    return x
+    return n
   end
   }
-  n
 end
 
 def find_min_value(array)
